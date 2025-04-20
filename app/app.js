ed2d5c87b0cb8caf8efb3163244f460c553d0728
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-04-20 19:12:41 +0530

    storing-data-in-file

--- app/app.js
+++ app/app.js
@@ -1,49 +1,50 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+
 const app = express();
 const port = 3000;
+
 let todos = [];
-// ✅ Body parser middleware
+
 app.use(express.json()); // parses JSON request body
 app.use(cors());
+app.post('/', async (req, res) => {
+    // *** req.body *** : { todo: 'IDC Website' }
+    let data = req.body
+    // its in js obj, we will convert that into
+    // string and put in file
+    console.log(" *** data *** :", data)
 
-app.post('/', (req, res) => {
-    let id = Math.floor(Math.random() * 101);
-    if (req.body !== '') {
-        todos.push(
-            id,
-            req.body
-        )
+    let file_content = fs.readFileSync('todo.json', 'utf-8')
+
+    if (file_content.trim()) {
+        todos = JSON.parse(file_content); // Convert string to array
+        console.log("todos (string to array) :", todos)
     }
-    res.send('Hello World!');
+    todos.push(data)
+    console.log("*** todos *** :", todos)
+
+    // Till here we checked if the file was empty, if it had content inside it, if it had then we parse the data and put that into the todos array, then we inserted or pushed the data we recieved as payload into the array.
+
+    // Now we will stringyfy the content and put that into the file
+    let str = JSON.stringify(todos)
+    console.log(" *** str *** :", str)
+
+    fs.writeFileSync('todo.json', str)
+
+
+
+    // todos.push({ data });
+    // console.log("todos :", todos)
+
+    // console.log(" *** file b *** :", file_content)
+    // file_content = JSON.parse(file_content)
+    // console.log(" *** file a *** :", file_content)
+    // fs.appendFile('todo.json', data, (err)=>{
+
+    // })
 })
-
-app.delete('/', (req, res) => {
-    if (req.body !== '') {
-        const data = req.body.textData
-        console.log(data)
-        const index = todos.findIndex((item) =>
-            item.todo === data);
-        console.log(index);
-        if (index !== -1) {
-            todos.splice(index, 1);
-            todos.splice(index-1, 1);
-        }
-        console.log("***todos***", todos)
-        res.send(todos);
-    }
-})
-
-app.get('/', (req, res) => {
-    res.json({
-        todos
-    })
-})
-
-app.delete('/', (req, res) => {
-
-})
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let inputButton = document.querySelector('#inputButton')
+let inputField = document.querySelector('#inputField')
 
 async function postData() {
-    let input = document.querySelector("#inputField").value
+    let input = inputField.value
     if (input !== '') {
+        console.log("**input** :", input)
         await fetch("http://localhost:3000/", {
             method: "POST",
             headers: {
@@ -12,76 +14,10 @@ async function postData() {
         })
     }
 }
-async function getData() {
-    const url = "http://localhost:3000/";
-    try {
-        const response = await fetch(url);
-        let resp = await (response.json())
-        todoFrontend(resp.todos)
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        // ...
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-function todoFrontend(array){
-    let length = array.length
-    emptyDiv()
-    for (let i = 1; i <= length; i++) {
-        if (i % 2 !== 0) {
-            createTodo(array[i].todo)
-        }
-    }
-}
-
-function createTodo(data) {
-    let todoDiv = document.createElement('div')
-    let todoCheck = document.createElement("input");
-    todoCheck.setAttribute("type", "checkbox");
-    let textContentElement = document.createElement('span')
-    textContentElement.textContent = data
-    let delBtn = document.createElement('button')
-    delBtn.textContent = "Delete"
-
-    todoDiv.appendChild(todoCheck)
-    todoDiv.appendChild(textContentElement)
-    todoDiv.appendChild(delBtn)
-    var inputSpan = document.querySelector("#todo-elements");
-    inputSpan.appendChild(todoDiv)
-    inputSpan.style.display = "flex"
-    inputSpan.style.flexDirection = "column"
-    todoDiv.style.display = "grid"
-    todoDiv.style.gridTemplateColumns = "150px 150px 150px"
-    todoDiv.style.gridAutoFlow = "column"
-    todoDiv.style.justifyContent = "center"
-    todoDiv.style.margin = "15px"
-    textContentElement.style.wordBreak = "break-word"; // bonus for very long strings
-
-    let textData = textContentElement.textContent
-    delBtn.addEventListener('click', async () => {
-        const res = await fetch("http://localhost:3000/", {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ textData })
-        })
-        const data = await res.json();
-        todoFrontend(data);
-    })
-}
-
-function emptyDiv() {
-    let ContentDiv = document.querySelector("#todo-elements")
-    ContentDiv.innerHTML = ""
-}
 
 inputButton.addEventListener('click', async () => {
     await postData();
-    await getData();
+    // await getData();
     // createTodo()
     document.querySelector("#inputField").value = ""
 })
