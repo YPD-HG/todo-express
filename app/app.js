3d4051728251954ef13723f1bc045a5c7ca32a82
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-04-21 14:28:17 +0530

    code-issue

--- app/app.js
+++ app/app.js
@@ -9,41 +9,47 @@ let todos = [];
 
 app.use(express.json()); // parses JSON request body
 app.use(cors());
+
+// app.post('/', (req, res) => {
+//     let id = Math.floor(Math.random() * 101);
+//     if (req.body !== '') {
+//         // todos.push(
+//         //     id,
+//         //     req.body
+//         // )
+//         console.log("Here***********")
+//     }
+//     res.send('Hello World!');
+// })
+
 app.post('/', async (req, res) => {
-    // *** req.body *** : { todo: 'IDC Website' }
-    let data = req.body
-    // its in js obj, we will convert that into
-    // string and put in file
-    console.log(" *** data *** :", data)
+    if (req.body !== '') {
+        // *** req.body *** : { todo: 'IDC Website' }
+        let data = req.body
+        // its in js obj, we will convert that into
+        // string and put in file
+        console.log(" *** data *** :", data)
 
-    let file_content = fs.readFileSync('todo.json', 'utf-8')
+        let file_content = fs.readFileSync('../data/todo.json', 'utf-8')
 
-    if (file_content.trim()) {
-        todos = JSON.parse(file_content); // Convert string to array
-        console.log("todos (string to array) :", todos)
+        if (file_content.trim()) {
+            todos = JSON.parse(file_content); 
+            // Convert string to array
+            console.log("todos (string to array) :", todos)
+        }
+        todos.push(data)
+        console.log("*** todos *** :", todos)
+
+        let str = JSON.stringify(todos)
+        console.log(" *** str *** :", str)
+
+        // something is wrong with writeFile
+        // fs.writeFile('../data/todo.json', str, (err) => {
+        //     if (err) throw err;
+        //     console.log('Data written');
+        // });
     }
-    todos.push(data)
-    console.log("*** todos *** :", todos)
-
-    // Till here we checked if the file was empty, if it had content inside it, if it had then we parse the data and put that into the todos array, then we inserted or pushed the data we recieved as payload into the array.
-
-    // Now we will stringyfy the content and put that into the file
-    let str = JSON.stringify(todos)
-    console.log(" *** str *** :", str)
-
-    fs.writeFileSync('todo.json', str)
-
-
-
-    // todos.push({ data });
-    // console.log("todos :", todos)
-
-    // console.log(" *** file b *** :", file_content)
-    // file_content = JSON.parse(file_content)
-    // console.log(" *** file a *** :", file_content)
-    // fs.appendFile('todo.json', data, (err)=>{
-
-    // })
+    res.send("Ok")
 })
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let inputField = document.querySelector('#inputField')
 async function postData() {
     let input = inputField.value
     if (input !== '') {
-        console.log("**input** :", input)
-        await fetch("http://localhost:3000/", {
+        let res = await fetch("http://localhost:3000/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({ todo: input })
         })
+        console.log("Hello here")
     }
 }
 
@@ -19,7 +19,7 @@ inputButton.addEventListener('click', async () => {
     await postData();
     // await getData();
     // createTodo()
-    document.querySelector("#inputField").value = ""
+    // document.querySelector("#inputField").value = ""
 })
 
 
