9cb1d10d72e2e03d27c2e0563adf8522416eb9ef
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-04-22 15:45:49 +0530

    final

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const port = 6001;
+const port = 3000;
 let todos = [];
 // ✅ Body parser middleware
 app.use(express.json()); // parses JSON request body
